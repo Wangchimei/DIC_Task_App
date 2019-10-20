a57292bc76be6555cf260bba4ae45a642365649a
Chi-Mei Wang <kimmy0929@gmail.com>
2019-10-20 16:26:49 +0900

    update index view

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,52 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:edit, :update, :show, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+      flash[:notice] = "タスクを作成しました"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @feed.update(task_params)
+      redirect_to tasks_path
+      flash[:notice] = "タスクが更新されました"
+    else
+      render :edit
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+    flash[:notice] = "タスクが削除されました"
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
+end
