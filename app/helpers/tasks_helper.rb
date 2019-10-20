789e4716a4d87936063511cf2910d87c35ab7b7f
Chi-Mei Wang <kimmy0929@gmail.com>
2019-10-20 17:11:06 +0900

    update new and edit view

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,9 @@
 module TasksHelper
+  def button_text
+    if action_name == "new" || action_name == "create" || action_name == "confirm"
+        return "作成する"
+    elsif action_name == "edit"
+        return "更新する"
+    end
+  end
 end
