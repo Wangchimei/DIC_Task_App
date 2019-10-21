aa0a9a13df5e7b0711210444b9ea162fdb025642
Chi-Mei Wang <kimmy0929@gmail.com>
2019-10-21 17:24:43 +0900

    set timezone as Tokyo

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module ELtraining
       g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
     # Settings in config/environments/* take precedence over those specified here.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
