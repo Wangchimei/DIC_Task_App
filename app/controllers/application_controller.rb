194f758ddedb14475d2fe9f8876fae75530b8878
Chi-Mei Wang <kimmy0929@gmail.com>
2019-10-22 16:32:42 +0900

    remove byebug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-  # before_action :basic if Rails.env == "production"
+  before_action :basic if Rails.env == "production"
 
-  # private
-  # def basic
-  #   authenticate_or_request_with_http_basic do |name, password|
-  #     name == ENV['admin'] && password == ENV['password']
-  #   end
-  # end
+  private
+  def basic
+    authenticate_or_request_with_http_basic do |name, password|
+      name == ENV['admin'] && password == ENV['password']
+    end
+  end
 end
--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-require 'byebug'
 
 RSpec.feature 'タスク管理機能', type: :feature do
   background do
