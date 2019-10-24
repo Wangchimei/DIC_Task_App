60eea7099e6767959c178380d0aa88d54e4a33f7
Chi-Mei Wang <kimmy0929@gmail.com>
2019-10-24 16:01:39 +0900

    udpate priority view

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
   validates :title, :content, :deadline, presence: true
   enum status: { not_started: 0, in_progress: 1, finished: 2}
+  enum priority: { low: 0, medium: 1, high: 2}
 
   scope :title_search, ->(title) { where("title LIKE ?", "%#{title}%") }
   scope :status_search, ->(state) { where(status: Task.statuses[state]) }
