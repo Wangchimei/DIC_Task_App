fb2ee6f12808c08eb2061af27a2e1aedc59ad6a4
Chi-Mei Wang <kimmy0929@gmail.com>
2019-10-24 15:23:44 +0900

    add index to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_23_143738) do
+ActiveRecord::Schema.define(version: 2019_10_24_062227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_10_23_143738) do
     t.datetime "updated_at", null: false
     t.datetime "deadline", default: -> { "now()" }, null: false
     t.integer "status", default: 0
+    t.index ["title", "status"], name: "index_tasks_on_title_and_status"
   end
 
 end
