c13e352227874fc6c19133e5c1f41bd17b0e13b6
Chi-Mei Wang <kimmy0929@gmail.com>
2019-10-25 19:11:49 +0900

    implement logout function and navbar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:session][:password])
       log_in
       redirect_to tasks_path
+      flash[:notice] = 'ログインしました'
     else
       render :new
     end
