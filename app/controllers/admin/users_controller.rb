46d6e8e5a2f50accc6c504946a84e4a0734e8ba5
Chi-Mei Wang <kimmy0929@gmail.com>
2019-10-27 01:01:15 +0900

    revise controller for admin toggles

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UsersController < ApplicationController
-  before_action :admin_user
   before_action :set_user, only: [:edit, :update, :toggle_admin, :destroy]
+  before_action :admin_user
 
   def index
     @users = User.all.order(created_at: :desc).page(params[:page])
@@ -33,9 +33,14 @@ class Admin::UsersController < ApplicationController
 
   def toggle_admin
     if current_user == @user
-      redirect_to tasks_path
-      @user.toggle!(:admin)
-      flash[:notice] = "管理者権限がなくなりました"
+      if rollback?
+        redirect_to admin_users_path
+        flash[:notice] = "権限を変更できません"
+      else
+        redirect_to tasks_path
+        current_user.toggle!(:admin)
+        flash[:notice] = "管理者権限がなくなりました"
+      end
     else
       @user.toggle!(:admin)
       redirect_to admin_users_path
@@ -66,5 +71,9 @@ class Admin::UsersController < ApplicationController
 
   def admin_user
     raise ActionController::RoutingError, "アクセスする許可がありません" unless current_user.admin?
+  end
+
+  def rollback?
+    User.where(admin:true).where.not(id: current_user.id).count == 0
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  # def rollback?
+  #   @users.where(admin:true).where.not(id: current_user.id).count == 0
+  # end
 end
