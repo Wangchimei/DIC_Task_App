55e4bf4a7c06ebaae6afa2a04de3c9dfb32edfe7
Chi-Mei Wang <kimmy0929@gmail.com>
2019-10-29 22:53:27 +0900

    modify factory bot

--- spec/factories/label_relations.rb
+++ spec/factories/label_relations.rb
@@ -1,16 +1,2 @@
 FactoryBot.define do
-  factory :label_relation do
-    task_id { 1 }
-    label_id { 1 }
-  end
-
-  factory :second_label_relation, class: LabelRelation do
-    task_id { 2 }
-    label_id { 1 }
-  end
-
-  factory :third_LabelRelation, class: LabelRelation do
-    task_id { 2 }
-    label_id { 2 }
-  end
 end
--- spec/factories/labels.rb
+++ spec/factories/labels.rb
@@ -1,11 +1,9 @@
 FactoryBot.define do
   factory :label do
-    # id { 1 }
     name { "遊び" }
   end
 
   factory :second_label, class: Label do
-    # id { 2 }
     name { "勉強" }
   end
 end
